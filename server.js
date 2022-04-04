8a9c61018da98e611618ca0a63ec97b56c0eca02
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-04-04 12:22:12 +0530

    Changed CSP report only back to CSP.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(function (req, res, next) {
   res.setHeader(
-    "Content-Security-Policy-Report-Only",
+    "Content-Security-Policy",
     "default-src 'self'; font-src 'self' https://fonts.gstatic.com; img-src 'self' https://images.unsplash.com; script-src 'self' https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/ 'sha256-INJfZVfoUd61ITRFLf63g+S/NJAfswGDl15oK0iXgYM='; style-src 'self' https://fonts.googleapis.com https://cdn.jsdelivr.net; frame-src 'self' https://www.youtube.com https://youtube.com;"
   );
   next();
