965c51d085351cc2b4754dfc523b7b8e29779fb1
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-04-04 12:01:04 +0530

    Image sources  were allowed.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(function (req, res, next) {
   res.setHeader(
     "Content-Security-Policy-Report-Only",
-    "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self' https://fonts.googleapis.com https://cdn.jsdelivr.net; frame-src 'self'"
+    "default-src 'self'; font-src 'self'; img-src 'self' https://images.unsplash.com; script-src 'self'; style-src 'self' https://fonts.googleapis.com https://cdn.jsdelivr.net; frame-src 'self'"
   );
   next();
 });
