2ff8358b1fde52748f59ee15e034c67ed8953d6b
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-04-04 12:19:52 +0530

    Inline sources were handled using HASH method.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(function (req, res, next) {
   res.setHeader(
     "Content-Security-Policy-Report-Only",
-    "default-src 'self'; font-src 'self' https://fonts.gstatic.com; img-src 'self' https://images.unsplash.com; script-src 'self' https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/; style-src 'self' https://fonts.googleapis.com https://cdn.jsdelivr.net; frame-src 'self' https://www.youtube.com https://youtube.com;"
+    "default-src 'self'; font-src 'self' https://fonts.gstatic.com; img-src 'self' https://images.unsplash.com; script-src 'self' https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/ 'sha256-INJfZVfoUd61ITRFLf63g+S/NJAfswGDl15oK0iXgYM='; style-src 'self' https://fonts.googleapis.com https://cdn.jsdelivr.net; frame-src 'self' https://www.youtube.com https://youtube.com;"
   );
   next();
 });
